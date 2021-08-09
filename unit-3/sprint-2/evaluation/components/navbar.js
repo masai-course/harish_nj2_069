c31162ee0e89eaf18fe34f41943708d561213b95
Harish069 <harish_nj2_069@masai.school>
2021-08-09 17:41:46 +0530

    ninth commit

--- unit-3/sprint-2/evaluation/components/navbar.js
+++ unit-3/sprint-2/evaluation/components/navbar.js
@@ -1,10 +1,11 @@
 function navbar()
 {
     return `<div id = "navbar">
+        <h1>Welcome to NEWS PORTAL</h1>
         <h3><a href = "./home.html">Home</a></h3>
 
         <div class="options">
-        <h3><a href="./search.html">search</a></h3>
+        <h3><a href="./search.html">Search</a></h3>
         </div>
     </div>
     <div id = "data1"></div>`
