7a11a523d557612102071e6769db0827f6c189f5
Harish069 <harish_nj2_069@masai.school>
2021-08-09 23:41:51 +0530

    My All Commits

---
+++ unit-3/sprint-2/day-4/you/components/navbar.js
@@ -0,0 +1,16 @@
+function navbar()
+{
+    return `<div id="navbar">
+            <h3><a href="./foodapp1.html">Home</a></h3>
+        
+            <div class="options">
+                 <h3><a href="./search.html">Search</a></h3>
+                <h3><a href="./day.html">Receipe of the day</a></h3>
+                <h3><a href="./latest.html">Latest Receipe</a></h3>
+            </div>
+        </div>
+        
+        <div id="data"></div>`
+} 
+
+export default navbar
