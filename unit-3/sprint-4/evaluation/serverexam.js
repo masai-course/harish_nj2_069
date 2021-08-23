9a74070182bc592f9c0f5d8d7631f1ca7cd972d6
Harish069 <harish_nj2_069@masai.school>
2021-08-23 14:40:44 +0530

    secondcommit

--- unit-3/sprint-4/evaluation/serverexam.js
+++ unit-3/sprint-4/evaluation/serverexam.js
@@ -13,6 +13,46 @@ const connect = () => {
     })
 }
 
+///////////Product Schema//////////////////
+
+const productSchema = new mongoose.Schema({
+    name: { type: String, require: true },
+    gender: [{ type: String, require: true }],
+    price: { type: Number, require: true },
+    colorId: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "color",
+        require: true,
+    }]
+}, {
+    versionKey:false
+})
+
+const Product = mongoose.model("product", productSchema)
+
+//////////Color Schema///////////////////
+
+const colorSchema = new mongoose.Schema({
+    colorname:{type:String, require:true}
+}, {
+    versionKey:false
+})
+
+const Color = mongoose.model("color", colorSchema)
+
+//////////////////////////////////////////////////////
+
+
+app.post("/prod", async (request, respond) => {
+    try {
+        const pro = await Product.create(request.body);
+        return respond.status(201).send(pro);
+    }
+    catch (err) {
+        return respond.status(400).send(err.message);
+    }
+});
+  
 
 app.listen(5858, async () => {
     await connect();
