85c58d456e6ef5f402a55edb9009f45819040e2a
Harish069 <harish_nj2_069@masai.school>
2021-09-13 15:00:45 +0530

    Third Commit

---
+++ unit-4/sprint-2/evaluation/grocery/src/components/DisplayItems.jsx
@@ -0,0 +1,7 @@
+export default function DisplayItems({ groc})
+{
+    const { title, status } = groc;
+    return (
+        <div>{ title }</div>
+  )
+}
--- unit-4/sprint-2/evaluation/grocery/src/components/topSection.jsx
+++ unit-4/sprint-2/evaluation/grocery/src/components/topSection.jsx
@@ -1,24 +1,27 @@
 import { useState } from "react"
 import { v4 as uuid } from "uuid"
+import DisplayItems from "./DisplayItems";
 export default function TopSection()
 {
     const [text, setText] = useState("");
-    const [grocerylist, setgrocerylist] = useState([]);
+    const [finalglist, setfinalglist] = useState([]);
+    const [wishglist, setwishglist] = useState([]);
     
     const handleChange = (e) => {
         setText(e.target.value);
         console.log(e.target.value);
     }
     const finallist = () => {
-        const payload = { title: text };
-        setgrocerylist([...grocerylist, payload]);
+        const payload = {status: true, title: text,id:uuid()};
+        setfinalglist([...finalglist, payload]);
     }
-     console.log(grocerylist);
+     console.log(finalglist);
 
      const wishlist = () => {
-        const payload = {status: document, title: text, id:uuid()};
-        setgrocerylist([...grocerylist, payload]);
+        const payload = {status: true, title: text, id:uuid()};
+        setwishglist([...wishglist, payload]);
     }
+    console.log(wishglist);
 
     return (
         <div>
@@ -26,6 +29,9 @@ export default function TopSection()
             <input onChange={handleChange} type="text" placeholder="enter groceries" />
             <button onClick={finallist}>Add to Final list</button>
             <button onClick={wishlist}>Add to Wish list</button>
+            {finalglist.map((e) => (
+             <DisplayItems key={e.id} groc={e}/>
+            ))}
             </div>
     )
 }
