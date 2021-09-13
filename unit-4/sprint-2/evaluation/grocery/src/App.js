a963ab5f6b3c124322a128a6df13fd45ab531f96
Harish069 <harish_nj2_069@masai.school>
2021-09-13 14:20:31 +0530

    First Commit

---
+++ unit-4/sprint-2/evaluation/grocery/src/App.js
@@ -0,0 +1,13 @@
+import TopSection from './components/topSection';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <TopSection />
+      {/* <bottomSection /> */}
+    </div>
+  );
+}
+
+export default App;
