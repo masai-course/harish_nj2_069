ac9ba2dbe28393c7178400ab5007f61842bd20e8
Harish069 <harish_nj2_069@masai.school>
2021-09-13 14:42:51 +0530

    Second commit

--- unit-4/sprint-2/evaluation/grocery/src/components/topSection.jsx
+++ unit-4/sprint-2/evaluation/grocery/src/components/topSection.jsx
@@ -1,7 +1,31 @@
-
+import { useState } from "react"
+import { v4 as uuid } from "uuid"
 export default function TopSection()
 {
+    const [text, setText] = useState("");
+    const [grocerylist, setgrocerylist] = useState([]);
+    
+    const handleChange = (e) => {
+        setText(e.target.value);
+        console.log(e.target.value);
+    }
+    const finallist = () => {
+        const payload = { title: text };
+        setgrocerylist([...grocerylist, payload]);
+    }
+     console.log(grocerylist);
+
+     const wishlist = () => {
+        const payload = {status: document, title: text, id:uuid()};
+        setgrocerylist([...grocerylist, payload]);
+    }
+
     return (
-        <div>Hello</div>
+        <div>
+            <h1>Add Groceries</h1>
+            <input onChange={handleChange} type="text" placeholder="enter groceries" />
+            <button onClick={finallist}>Add to Final list</button>
+            <button onClick={wishlist}>Add to Wish list</button>
+            </div>
     )
 }
