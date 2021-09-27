abacf3bc9b0a4fb1970766662933bbeaf6bcba02
Harish069 <harish_nj2_069@masai.school>
2021-09-27 16:29:49 +0530

    Book Store

--- unit-4/sprint-4/evaluation/store/src/components/Books.jsx
+++ unit-4/sprint-4/evaluation/store/src/components/Books.jsx
@@ -1,29 +1,40 @@
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
-import  axios  from "axios";
+import "./books.css";
+
 
 function Books()
 {
     const [books, setBook] = useState([]);
 
     useEffect(() => {
-        axios.get("https://localhost:3001/books").then((res) => {
-            console.log(res.data);
-            setBook(res.data.data);
+        fetch("http://localhost:3001/books").then((data) => data.json())
+        .then((data) => {
+            console.log(data);
+            setBook(data);
         });
     }, [])
+    console.log(books);
 
     return (
         <div>
             <ul>
+                <div className="main" >
                 {books.map((e) => (
-                    <li
-                        key={e.id}>
-                        <Link to = {`/Book/${e.id}`}>
-                            {e.Title}
+                        <Link to={`/Book/${e.id}`}>
+                        <div className="parent" key={e.id}>
+                            <img className="image" src={e.image} />
+                            
+                            <div className="fchild">
+                                <div className="Name"> Title: {e.Title}</div>
+                                <div className="desc">Descirption: {e.description}</div>
+                              <div className="rating">  Rating: {e.rating}</div>
+                             <div className="author">Author:{e.author}</div>
+                           </div>
+                            </div>
                         </Link>
-                    </li>
                 ))}
+                </div>
             </ul>
         </div>
     )
