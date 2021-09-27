73b94688083492cabb7c5784ab785a71119cc094
Harish069 <harish_nj2_069@masai.school>
2021-09-27 16:48:44 +0530

    Big Store

--- unit-4/sprint-4/evaluation/store/src/components/Books.jsx
+++ unit-4/sprint-4/evaluation/store/src/components/Books.jsx
@@ -6,19 +6,26 @@ import "./books.css";
 function Books()
 {
     const [books, setBook] = useState([]);
+    const [page, setPage] = useState(1);
 
     useEffect(() => {
-        fetch("http://localhost:3001/books").then((data) => data.json())
+        fetch(`http://localhost:3001/books?_page=${page}&_limit=10`)
+        .then((data) => data.json())
         .then((data) => {
             console.log(data);
             setBook(data);
         });
-    }, [])
+    }, [page])
+
     console.log(books);
 
     return (
         <div>
             <ul>
+                {"Page-no:"}
+                {page}
+                 <button onClick={() => setPage(page - 1)}>Prev Page</button>
+                <button onClick={() => setPage(page + 1)}>Next Page</button>
                 <div className="main" >
                 {books.map((e) => (
                         <Link to={`/Book/${e.id}`}>
@@ -28,7 +35,7 @@ function Books()
                             <div className="fchild">
                                 <div className="Name"> Title: {e.Title}</div>
                                 <div className="desc">Descirption: {e.description}</div>
-                              <div className="rating">  Rating: {e.rating}</div>
+                                <div className="rating">  Rating: {e.rating}</div>
                              <div className="author">Author:{e.author}</div>
                            </div>
                             </div>
